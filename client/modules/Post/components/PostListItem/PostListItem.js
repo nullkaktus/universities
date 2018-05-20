650a456e83091ea11921a7f9fc59cba50748d25c
nullkaktus <diana.birenbaum@gmail.com>
2018-05-20 20:11:15 +0200

    Simplified list view
    Deleted "by", button "delete post", "add post" in the interface (but not their implementations)

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -13,9 +13,8 @@ function PostListItem(props) {
           {props.post.title}
         </Link>
       </h3>
-      <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
+      <p className={styles['author-name']}>{props.post.name}</p>
       <p className={styles['post-desc']}>{props.post.country}, {props.post.city}</p>
-      <p className={styles['post-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
       <hr className={styles.divider} />
     </div>
   );
